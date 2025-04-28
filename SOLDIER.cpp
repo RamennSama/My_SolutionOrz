48b0be3b2e6ea41d870dc8ccdc3f0f8867986839
LongPham <135238591+RamennSama@users.noreply.github.com>
2025-04-28 09:01:42 +0700

    Up Solution

--- SOLDIER.cpp
+++ SOLDIER.cpp
@@ -21,6 +21,40 @@ typedef unsigned long long ull;
 const int N = 1e5 + 15;
 const int oo = 1e9;
 
+// tag : DP
+int n, m, ans = 0;
+vector<ii>a[106];
+vector<vector<int>>f(7, vector<int>(1006, -1));
+
+void solve() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        a[x].push_back(ii(y, z));
+    }
+    for(int i = 0; i < a[1].size(); i++) {
+        f[1][a[1][i].fi] = max(f[1][a[1][i].fi], a[1][i].se);
+    }
+    for(int i = 2; i <= 6; i++) {
+        for(int j = 0; j < a[i].size(); j++) {
+            for(int t = a[i][j].fi; t <= m; t++) {
+                if(f[i - 1][t - a[i][j].fi] != -1) {
+                    f[i][t] = max(f[i][t], min(f[i - 1][t - a[i][j].fi], a[i][j].se));
+                }
+            }
+        }
+    }
+    for(int t = 0; t <= m; t++) {
+        if(f[6][t] != -1) {
+            ans = max(ans, f[6][t]);
+        }
+    }
+    cout << ans << endl;
+}
+// f[6][t] != -1 : la chat luong nho nhat cua ptu ben trong cac mon do tu 1 -> 6 voi tong la t
+
+// tag : greedy
 void solve() {
     int n, m, res = 0, total = 0, v;
     cin >> n >> m;
